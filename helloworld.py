d04346272aadf177934e17ff3fcd095db94c39a0
Liwei <chengli@uwplatt.edu>
2023-07-16 14:59:48 -0500

    try this string format

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,13 @@ parser.add_argument('--radVariable', type=str, required=True)
 
 args = parser.parse_args()
 
-print('Email:', args.adminEmail)
-print('coolVariable:', args.coolVariable)
-print('radVariable:', args.radVariable)
+# print('Email:', args.adminEmail)
+# print('coolVariable:', args.coolVariable)
+# print('radVariable:', args.radVariable)
+
+adminEmail = args.adminEmail
+coolVariable = args.coolVariable
+radVariable = args.radVariable
+
+print(f'This is the email: adminEmail')
 
